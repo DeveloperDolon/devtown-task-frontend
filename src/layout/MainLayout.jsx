2ab64d3fa9afb7e80939dcc09bc7fbfb0908fb9a
Dolon Roy <dolonr718@gmail.com>
2023-12-20 07:46:23 +0600

    banner slider added commit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -18,7 +18,7 @@ const MainLayout = () => {
                 <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col">
                     {/* Navbar */}
-                    <div className="w-full navbar bg-base-300">
+                    <div className="w-full navbar bg-[#c8d6e5]">
                         <div className="flex-none lg:hidden">
                             <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -40,7 +40,7 @@ const MainLayout = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200 font-medium">
+                    <ul className="menu p-4 w-80 min-h-full bg-[#c8d6e5] font-medium">
                         {/* Sidebar content here */}
                         {navBars}
                     </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,72 @@
-
-
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import "./style.css"
 const Home = () => {
     return (
-        <div className="container mx-auto md:px-0 sm:px-5 px-3">
-            Hello world from home page. asdf
+        <div>
+
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 4000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper w-[calc(100vw)] mx-auto h-[calc(100vh-76px)] relative"
+            >
+                <SwiperSlide>
+                    <div className='absolute top-[40%] left-28'>
+                        <div className='deal-box p-5 text-white rounded-lg mb-5 md:text-2xl text-lg font-bold w-fit'>
+                            <h1>Mega Deal</h1>
+                        </div>
+
+                        <h1 className='md:text-6xl text-4xl font-bold  text-white pb-7'>
+                            Offer up to 50%
+                        </h1>
+                        <button className='btn btn-primary'>Buy Now</button>
+                    </div>
+                    <img className='w-full ' src="https://images.samsung.com/is/image/samsung/assets/us/12172023/Fold5_Dec_Offer_ETI_Cyber_Week_HP-Hero-Carousel-DT-1440x640.jpg?imwidth=1366" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='absolute top-[40%] left-28'>
+                        <div className='deal-box p-5 text-white rounded-lg mb-5 md:text-2xl text-lg font-bold w-fit'>
+                            <h1>Mega Deal</h1>
+                        </div>
+
+                        <h1 className='md:text-6xl text-4xl font-bold  text-white pb-7'>
+                            Offer up to 55%
+                        </h1>
+                        <button className='btn btn-primary'>Buy Now</button>
+                    </div>
+                    <img className='w-full' src="https://images.samsung.com/is/image/samsung/assets/bd/2307/home/default-HOME_Q5_MX-KV_1440X810_pc.jpg?$1440_810_JPG$" alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='absolute top-[40%] left-28'>
+                        <div className='deal-box p-5 text-white rounded-lg mb-5 md:text-2xl text-lg font-bold w-fit'>
+                            <h1>Mega Deal</h1>
+                        </div>
+
+                        <h1 className='md:text-6xl text-4xl font-bold  text-white pb-7'>
+                            Offer up to 40%
+                        </h1>
+                        <button className='btn btn-primary'>Buy Now</button>
+                    </div>
+                    <img className='w-full' src="https://fdn.gsmarena.com/imgroot/static/headers/makers/apple-2023-1.jpg" alt="" />
+                </SwiperSlide>
+            </Swiper>
+
+            <div className="container mx-auto md:px-0 sm:px-5 px-3">
+
+            </div>
         </div>
     );
 };
