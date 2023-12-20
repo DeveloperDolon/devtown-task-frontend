29158edb05d944d6a118e62001ccd56fb1522be8
Dolon Roy <dolonr718@gmail.com>
2023-12-20 06:48:37 +0600

    nav bar added commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@
 
 const Home = () => {
     return (
-        <div>
-            Hello world from home page.
+        <div className="container mx-auto md:px-0 sm:px-5 px-3">
+            Hello world from home page. asdf
         </div>
     );
 };
